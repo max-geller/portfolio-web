b8ba0199e5e7394ead4daae41b734ef800a232f4
Max Geller <mxgeller@gmail.com>
2024-08-11 18:17:55 -0700

    updating source for images

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["storage.cloud.google.com", "storage.googleapis.com", "images.pexels.com"],
+    domains: [
+      "storage.cloud.google.com",
+      "storage.googleapis.com",
+      "images.pexels.com",
+      "firebasestorage.googleapis.com",
+    ],
   },
 };
 
