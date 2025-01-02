2aa664e6abf2bc178322f0ad106fd457ac361b1d
Max Geller <mxgeller@gmail.com>
2025-01-01 19:37:09 -0700

    adding firebase-sample

---
+++ app/firebase-sample.ts
@@ -0,0 +1,17 @@
+// Rename this file to firebase.ts and add your own firebase config
+
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
---
+++ app/travel/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+import Image from "next/image";
+import TravelGrid from "./../_components/TravelGrid";
+
+export default function TravelHome() {
+  return (
+    <main className="flex flex-col">
+      <TravelGrid />
+    </main>
+  );
+}
