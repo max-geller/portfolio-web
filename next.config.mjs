11d794f3063b966dfdcf0009346935e5f61d112c
Max Geller <mxgeller@gmail.com>
2025-01-04 21:56:50 -0700

    misc updates, debugging

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,31 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: [
-      "storage.cloud.google.com",
-      "storage.googleapis.com",
-      "images.pexels.com",
-      "firebasestorage.googleapis.com",
-      
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "storage.cloud.google.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "images.pexels.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "firebasestorage.googleapis.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "storage.googleapis.com",
+        port: "",
+        pathname: "/**",
+      },
     ],
   },
 };
